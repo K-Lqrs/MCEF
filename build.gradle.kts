1cfbb90df6b8323d601f2487517527b89b4227b1
Ruxy <tomatotouroku0001@gmail.com>
2024-06-08 18:09:46 +0900

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id ("maven-publish")
+    id ("fabric-loom")
+}
+
+repositories {
+
+}
+
+dependencies {
+    val minecraftVersion: String by project
+    val yarnMappings: String by project
+
+    minecraft("com.mojang:minecraft:$minecraftVersion")
+    mappings("net.fabricmc:yarn:$yarnMappings")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            groupId = "net.rk4z"
+            artifactId = "mcef"
+            version = "1.0.0"
+
+            pom {
+                name.set("MCEF")
+                description.set("A Minecraft API to embed a Chromium browser")
+                url.set("https://github.com/yourusername/mcefapi")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yourusername")
+                        name.set("Your Name")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/yourusername/mcefapi.git")
+                    developerConnection.set("scm:git:ssh://github.com/yourusername/mcefapi.git")
+                    url.set("https://github.com/yourusername/mcefapi")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
